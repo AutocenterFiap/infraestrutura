5c7d26e5db34d615491c6ffe03ee61e1c72580f1
Felipe Neaime <gitlab_admin_c8ff20@example.com>
2026-08-21 22:00:31 -0300

    Adiciona criação de ERC

--- output.tf
+++ output.tf
@@ -13,3 +13,8 @@ output "subnet_cidr" {
 output "subnet_id" {
   value = aws_subnet.subnet_public[*].id
 }
+
+output "ecr_repository_url" {
+  description = "URL do repositorio ECR usado para publicar a imagem Docker da aplicacao"
+  value       = aws_ecr_repository.autocenter.repository_url
+}
