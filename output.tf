f2e912fdc802215f8ba7227b2740d5cc74f90694
Felipe Neaime <gitlab_admin_c8ff20@example.com>
2026-09-13 19:48:46 -0300

    Ajusta output para ser consultado pelo repositorio do gateway.

--- output.tf
+++ output.tf
@@ -18,3 +18,13 @@ output "ecr_repository_url" {
   description = "URL do repositorio ECR usado para publicar a imagem Docker da aplicacao"
   value       = aws_ecr_repository.autocenter.repository_url
 }
+
+output "eks_cluster_name" {
+  description = "Nome do cluster EKS"
+  value       = aws_eks_cluster.cluster.name
+}
+
+output "private_subnet_ids" {
+  description = "IDs das subnets privadas / subnets do cluster"
+  value       = aws_subnet.subnet_public[*].id
+}
